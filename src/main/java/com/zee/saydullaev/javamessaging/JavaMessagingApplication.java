6e03546fc61a8b417124e5b3955894b03c9de245
DudeCalledZee <ineedtothink@gmail.com>
2021-07-02 10:19:24 +0100

    init proj

---
+++ src/main/java/com/zee/saydullaev/javamessaging/JavaMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.zee.saydullaev.javamessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMessagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zee/saydullaev/javamessaging/JavaMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee.saydullaev.javamessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
